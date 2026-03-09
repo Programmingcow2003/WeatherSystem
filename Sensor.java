3962d7051f6d2b3565e2016d8bfbe6302764e734
Programmingcow2003 <154718480+Programmingcow2003@users.noreply.github.com>
2026-03-08 22:46:14 -0400

    Implement continuous signal strength updates
    Added continuous signal strength testing with bounds checking.

--- Sensor.java
+++ Sensor.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.lang.Math;
 
 public class Sensor
   {
@@ -8,9 +9,26 @@ public class Sensor
       //Sets up random class and initalizes double
       Random rand = new Random();
       double signalStrength = rand.nextDouble() * 10;
+      
 
-      //Tests signal a few time
-      System.out.println(signalStrength);
+      //This will eventually be continuous but want it to run through
+      for(int i = 0; i < 100; i++)
+      {
+
+        //Signal can change between -0.5.0.5
+        double change = - 0.5 + rand.nextDouble();
+        signalStrength += change;
+
+        //Makes sure signal stays in bounds
+        if(signalStrength > 10)
+          signalStrength = 10;
+
+        if(signalStrength < 0)
+          signalStrength = 0;
+
+        //Prints signal strength
+        system.out.println(signalStrength);
+      }
 
       
     }

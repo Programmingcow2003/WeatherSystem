ba36b4b0adcbe64d9d7460ababea0fb94cfdbe6a
Programmingcow2003 <154718480+Programmingcow2003@users.noreply.github.com>
2026-03-15 20:56:26 -0400

    Add files via upload

--- sampler.js
+++ sampler.js
@@ -1,17 +1,19 @@
-function sampleData(voltage, amount) {
+function sampleData(voltage, amount) 
+{
   let result = [];
-  let sample = [];
+  let sample = [...voltage];
 
-  //Sets the amount it will send (if sample is too small won't send)
-  amount = Math.min(amount, sample.length);
+  //Sets the amount it will send (if sample is too small won't send extra to cause it to break)
+  let number_to_choose = Math.min(amount, sample.length);
 
-  //Pictures a random set of the numbers
-  for (let i = 0; i < amount; i++) {
-    let index = Math.floor(Math.random() * sample.length);
+  //Picks a random set of the numbers
+  for (let i = 0; i < number_to_choose; i++) 
+  {
+    let random_choice = Math.floor(Math.random() * sample.length);
 
     //Puts the results and gets a certain amount of samples
-    result.push(sample[index]);
-    sample.splice(index, 1);
+    result.push(sample[random_choice]);
+    sample.splice(random_choice, 1);
   }
 
   return result;
--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const http = require('http');
 const sampleData = require('./sampler');
 
-http.createServer((req, res) => {
-  if (req.method === 'POST' && req.url === '/sample') {
+const server = http.createServer((req, res) => 
+  {
+  if (req.method === 'POST' && req.url === '/sample') 
+  {
     let body = '';
 
     req.on('data', chunk => body += chunk);
 
-    req.on('end', () => {
+    req.on('end', () => 
+    {
       const data = JSON.parse(body);
 
       const sample = sampleData(data.voltage, data.amount);
 
       res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({
+      res.end(JSON.stringify(
+      {
         status: "received",
         sample: sample
       }));
     });
   }
-}).listen(3000);
+});
+
+server.listen(3000);
 
 console.log("Server running on http://localhost:3000");
+
+module.exports = server;
